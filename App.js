6e6f8c5cb7b015b83273722fff16b15309533ecd
Vignesh Raman <114500150+vickythecode@users.noreply.github.com>
2024-09-06 21:08:28 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
